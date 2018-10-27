4065687dcb8ae9f5d32be959d6b457380f474a4d
Jenky Nolasco <jenky.nolasco@gmail.com>
2018-10-27 15:22:54 -0400

    reworked app structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const App = () => {
+    return (
+        <View style={{ flex : 1, alignItems : 'center', justifyContent : 'center' }}>
+            <Text> Hi </Text>
+        </View>
+    )
+}
+
+export default App
